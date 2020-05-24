8b8cd239a584b7a0625351f15cd198569150fa61
zosia <zofiaanna000@gmail.com>
2020-05-24 20:44:24 +0200

    clickable songs/albums/authors/feats

--- src/app/components/results-list-element/results-list-element.component.ts
+++ src/app/components/results-list-element/results-list-element.component.ts
@@ -25,28 +25,6 @@ export class ResultsListElementComponent implements OnChanges {
       this.albumURL = null;
   }
 
-  formatFeatsNames(feats: any[]): string {
-    let text = '';
-    for (let i = 0; i < feats.length; i++) {
-      text += feats[i].name + ', ';
-    }
-    if (text.length > 0) {
-      text = text.substr(0, text.length - 2);
-    }
-    return 'Featuring: ' + text;
-  }
-
-  formatGenres(genres: any[]): string {
-    let text = '';
-    for (let i = 0; i < genres.length; i++) {
-      text += genres[i] + ', ';
-    }
-    if (text.length > 0) {
-      text = text.substr(0, text.length - 2);
-    }
-    return 'Genres: ' + text;
-  }
-
   changeSource(event) {
     event.target.src = 'assets/no-image-available.png';
   }
